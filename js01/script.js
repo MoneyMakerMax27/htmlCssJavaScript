7d03b64b240de12eafa87044b5befb3e542d15a6
MoneyMakerMax27 <mc_mrobertson27@mcjvs2.org>
2025-10-14 10:18:21 -0400

    october 14 update

---
+++ js01/script.js
@@ -0,0 +1,3 @@
+//Maxwell Robertson
+console.log("External file loaded!");
+document.write("<p>Today I learned how to link external JavaScript files to HTML and use basic output methods like console.log and document.write to display messages and content on a webpage.</p>");
